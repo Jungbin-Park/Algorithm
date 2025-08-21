c21ed96bd1f763512f6620740d7bb77db989c6e0
Majangnan <83396015+Jungbin-Park@users.noreply.github.com>
2025-08-21 17:59:02 +0900

    [level 3] Title: 네트워크, Time: 0.05 ms, Memory: 4.18 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -1,40 +1,38 @@
 #include <iostream>
-#include <string>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
-// 모든 컴퓨터가 간접적으로 연결되어 있으면 하나의 네트워크
-// dfs 로 방문체크 하면서 순환 -> dfs 횟수 = 네트워크 개수
+vector<bool> visited;
 
-bool visited[200];
-int answer = 0;
-
-void dfs(const vector<vector<int>>& computers, int startNode)
+void dfs(int start, const vector<vector<int>>& computers)
 {
-    if (visited[startNode]) return;
+	visited[start] = true;
 
-    visited[startNode] = true;
-
-    for (int i = 0; i < computers.size(); i++)
-    {
-        if (computers[startNode][i])
-        {
-            dfs(computers, i);
-        }
-    }
+	for (int i = 0; i < computers[start].size(); i++)
+	{
+		if (computers[start][i] && !visited[i])
+		{
+			dfs(i, computers);
+		}
+	}
 }
 
-int solution(int n, vector<vector<int>> computers) 
+int solution(int n, vector<vector<int>> computers)
 {
-    for (int i = 0; i < n; i++)
-    {
-        if (!visited[i])
-        {
-            dfs(computers, i);
-            answer++;
-        }
-    }
+	int result = 0;
 
-    return answer;
+	visited.resize(n, false);
+
+	for (int i = 0; i < n; i++)
+	{
+		if (!visited[i])
+		{
+			dfs(i, computers);
+			result++;
+		}
+	}
+
+	return result;
 }
