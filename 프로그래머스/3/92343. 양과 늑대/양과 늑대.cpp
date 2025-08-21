9a4d8f2031be03349cf61e52e68eb739822e6110
Majangnan <83396015+Jungbin-Park@users.noreply.github.com>
2025-08-21 20:54:51 +0900

    [level 3] Title: 양과 늑대, Time: 1.73 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.cpp
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.cpp
@@ -1,64 +1,63 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
+int n, answer = 0;
 vector<vector<int>> tree;
-vector<bool> visited;
 
-int n = 0, answer = 0;
+vector<int> visited;
 
-void dfs(const vector<int>& info, vector<int> cur)
+void dfs(vector<int> cur, const vector<int>& info)
 {
-    int sheep = 0, wolf = 0;
+	int sheepCount = 0;
+	int wolfCount = 0;
 
-    // 현재 경로에서 카운팅
-    for (int c : cur)
-    {
-        if (info[c] == 1) wolf++;
-        else sheep++;
-    }
+	// 현재 경로를 기준으로 양/늑대 카운트
+	for (int c : cur)
+	{
+		if (info[c] == 1) wolfCount++;
+		else sheepCount++;
+	}
 
-    // 늑대가 같거나 많으면 리턴
-    if (sheep <= wolf) return;
+	if (sheepCount <= wolfCount) return;
 
-    answer = max(answer, sheep);
+	// 최대 양의 수 갱신
+	answer = max(answer, sheepCount);
 
-    for (int i = 0; i < cur.size(); i++)
-    {
-        int node = cur[i];
+	for (int i = 0; i < cur.size(); i++)
+	{
+		int node = cur[i];
+		// 현재 노드와 인접한 노드 순회
+		for (const auto& neighbor : tree[node])
+		{
+			// 방문한 노드는 건너뜀
+			if (visited[neighbor]) continue;
 
-        for (int neighbor : tree[node])
-        {
-            if (visited[neighbor]) continue;
-
-            visited[neighbor] = true;
-            cur.push_back(neighbor);
-            dfs(info, cur);
-            cur.pop_back();
-            visited[neighbor] = false;
-        }
-    }
-    
+			// 방문 체크, 경로에 노드 추가 후 dfs 호출
+			visited[neighbor] = true;
+			cur.push_back(neighbor);
+			dfs(cur, info);
+			cur.pop_back();
+			visited[neighbor] = false;
+		}
+	}
 }
 
-int solution(vector<int> info, vector<vector<int>> edges) 
+int solution(vector<int> info, vector<vector<int>> edges)
 {
-    n = info.size();
+	n = info.size();
+	tree.resize(n);
+	visited.resize(n, false);
 
-    tree.resize(n);
-    visited.resize(n);
+	for (const auto& e : edges)
+	{
+		tree[e[0]].push_back(e[1]);
+	}
 
-    for (int i = 0; i < edges.size(); i++)
-    {
-        int node = edges[i][0];
-        int neighbor = edges[i][1];
+	visited[0] = true;
+	dfs({ 0 }, info);
 
-        tree[node].push_back(neighbor);
-    }
-
-    visited[0] = true;
-    dfs(info, { 0 });
-
-    return answer;
+	return answer;
 }
